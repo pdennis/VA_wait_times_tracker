4427377928d904f7fdbdf6bb69c8ac9062ebd715
Dave Swindell <cdswindell@users.noreply.github.com>
2025-05-29 11:15:45 -0400

    Added shell script to run download_reports.bash

--- scripts/shell/download_reports.bash
+++ scripts/shell/download_reports.bash
@@ -3,5 +3,7 @@
 cd /Users/davids/Documents/dev/BridgeEnv/VA_wait_times_tracker
 source ../bin/activate
 export PYTHONPATH=.
+touch running_download_reports
 python cli/download_reports.py
+rm -fr running_download_reports
 touch download_reports_last_run
