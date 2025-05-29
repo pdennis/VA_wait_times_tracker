c2d4361b7f4f77c0cd79b68449447f346eaba676
Dave Swindell <cdswindell@users.noreply.github.com>
2025-05-29 14:29:53 -0400

    Added shell script to run download_reports.bash

--- scripts/shell/download_reports.bash
+++ scripts/shell/download_reports.bash
@@ -7,6 +7,6 @@ source ../bin/activate
 export PYTHONPATH=.
 touch running_download_reports
 echo $PATH
-../bin/python cli/download_reports.py
+../bin/python --version
 rm -fr running_download_reports
 touch download_reports_last_run
